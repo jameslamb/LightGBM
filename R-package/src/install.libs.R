d3b4862b0cb56f3bc9b738da8e37ecd45ca9bba9
James Lamb <jaylamb20@gmail.com>
2020-04-02 19:07:47 -0500

    removing cache

--- R-package/src/install.libs.R
+++ R-package/src/install.libs.R
@@ -29,6 +29,11 @@ if (!(R_int_UUID == "0310d4b8-ccb1-4bb8-ba94-d36a55f60262"
   for (vs_version in vs_versions) {
     print(sprintf("Trying '%s'", vs_version))
     build_dir <- tempdir()
+    if (dir.exists(build_dir)){
+      print(sprintf("Directory '%s' already exists, removing it", build_dir))
+      unlink(build_dir, recursive = TRUE, force = TRUE)
+      build_dir <- tempdir()
+    }
     setwd(build_dir)
     writeLines(
       text = "PROJECT(testing)"
