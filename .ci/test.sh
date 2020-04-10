02d3bf560138989c6cc3a1526699132f641959ed
James Lamb <jaylamb20@gmail.com>
2020-04-09 23:03:31 -0500

    pegging r-lintr

--- .ci/test.sh
+++ .ci/test.sh
@@ -55,7 +55,7 @@ if [[ $TRAVIS == "true" ]] && [[ $TASK == "lint" ]]; then
         --channel conda-forge \
         --strict-channel-priority \
             libxml2 \
-            r-lintr>=2.0
+            r-lintr==2.0.0
     pip install --user cpplint
     echo "Linting Python code"
     pycodestyle --ignore=E501,W503 --exclude=./compute,./.nuget . || exit -1
