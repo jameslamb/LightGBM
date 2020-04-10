5a236af9dd6d03cc59868fb3ad9ee518fc6ca858
James Lamb <jaylamb20@gmail.com>
2020-04-09 23:46:18 -0500

    plz work

--- .ci/test.sh
+++ .ci/test.sh
@@ -55,7 +55,7 @@ if [[ $TRAVIS == "true" ]] && [[ $TASK == "lint" ]]; then
         --channel conda-forge \
         --strict-channel-priority \
             libxml2
-    Rscript -e "install.packages('lintr', repos = 'https://cloud.r-project.org/')"
+    Rscript -e "install.packages('lintr', repos = 'https://cloud.r-project.org/', dependencies = c('Depends', 'Imports', 'LinkingTo'))"
     pip install --user cpplint
     echo "Linting Python code"
     pycodestyle --ignore=E501,W503 --exclude=./compute,./.nuget . || exit -1
