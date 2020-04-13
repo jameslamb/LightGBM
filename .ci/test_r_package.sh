77c27ce9dc75d3196221e46f45a52a0cfffc6f8c
James Lamb <jaylamb20@gmail.com>
2020-04-12 21:36:36 -0500

    more updates to CI

--- .ci/test_r_package.sh
+++ .ci/test_r_package.sh
@@ -35,6 +35,7 @@ fi
 # Installing R precompiled for Mac OS 10.11 or higher
 if [[ $OS_NAME == "macos" ]]; then
 
+    # Azure Mac images aren't built with the macOS SDK headers enabled
     if [[ $AZURE == "true" ]] && [[ $R_BUILD_TYPE == "cran" ]]; then
         sudo installer \
             -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg \
