068517328168a274fdf094e3afa8393cc936ee52
James Lamb <jaylamb20@gmail.com>
2020-04-12 19:33:19 -0500

    removed mac headers thing

--- .ci/test_r_package.sh
+++ .ci/test_r_package.sh
@@ -35,9 +35,9 @@ fi
 # Installing R precompiled for Mac OS 10.11 or higher
 if [[ $OS_NAME == "macos" ]]; then
 
-    sudo installer \
-        -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg \
-        -target /
+    # sudo installer \
+    #     -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg \
+    #     -target /
 
     brew install automake
     
