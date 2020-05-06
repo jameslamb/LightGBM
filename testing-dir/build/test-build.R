4c5c3f34f08487a805f54b4ce35e777640a20770
James Lamb <jaylamb20@gmail.com>
2020-05-06 16:44:50 -0500

    trying with shell()

--- testing-dir/build/test-build.R
+++ testing-dir/build/test-build.R
@@ -1,11 +1,22 @@
 
-exitCode <- system2(
-    command="cmake"
-    , args = c(
+# exitCode <- system2(
+#     command="cmake"
+#     , args = c(
+#         paste0("-G", shQuote("Visual Studio 15 2017"))
+#         , "-A"
+#         , "x64"
+#         , " .."
+#     )
+# )
+
+exitCode <- shell(
+    cmd = paste0(
+        "cmake "
         paste0("-G", shQuote("Visual Studio 15 2017"))
         , "-A"
         , "x64"
-        , ".."
+        , " .."
     )
+    , intern = FALSE
 )
 print(exitCode)
