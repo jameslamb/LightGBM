2fda5561b686b8db8c5743878b0964ea49ea8366
James Lamb <jaylamb20@gmail.com>
2020-05-06 22:19:07 -0500

    system2

--- testing-dir/build/test-build.R
+++ testing-dir/build/test-build.R
@@ -39,6 +39,24 @@ print('----- sys.which -----')
 print(Sys.which("cmake"))
 install.packages('processx', repos = 'http://cran.rstudio.com')
 
+print('----- processx PATH -----')
+print(processx::run("echo", args = "$PATH"))
+
+print('----- Sys.getenv() PATH -----')
+print(Sys.getenv()[["PATH"]])
+
+# print('----- system2() -----')
+# exitCode <- system2(
+#     command="cmake"
+#     , args = c(
+#         paste0("-G", shQuote("Visual Studio 15 2017"))
+#         , "-A"
+#         , "x64"
+#         , ".."
+#     )
+# )
+# print(exitCode)
+
 # print('---- processx -----')
 # processx::run(
 #     command = "where"
