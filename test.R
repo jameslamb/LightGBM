2436ec03e95105ca36bf04852a26fb252e812de3
James Lamb <jaylamb20@gmail.com>
2021-08-01 04:44:44 +0100

    test case with lgb.cv()

--- test.R
+++ test.R
@@ -4,11 +4,24 @@ library(fansi)
 library(lightgbm)
 
 # create a dataset from a regular R matrix
-dtrain <- lgb.Dataset(
-    data = matrix(rnorm(1000), nrow = 100)
-    , label = rnorm(100)
-)
-print("constructing")
-dtrain$construct()
+# dtrain <- lgb.Dataset(
+#     data = matrix(rnorm(1000), nrow = 100)
+#     , label = rnorm(100)
+# )
+# print("constructing")
+# dtrain$construct()
+# 
+# print("done")
 
+
+data(agaricus.train, package='lightgbm')
+train <- agaricus.train
+dtrain <- lgb.Dataset(train$data, label = train$label)
+model <- lgb.cv(
+    params = list(
+        objective = "regression"
+        , metric = "l2"
+    )
+    , data = dtrain
+)
 print("done")
