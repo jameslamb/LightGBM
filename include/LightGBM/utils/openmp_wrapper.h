2ff860873ab227ee8b44a8886d646b831e2712b1
James Lamb <jaylamb20@gmail.com>
2023-10-11 16:51:52 -0500

    trying stuff

--- include/LightGBM/utils/openmp_wrapper.h
+++ include/LightGBM/utils/openmp_wrapper.h
@@ -19,11 +19,11 @@
 #include <vector>
 
 // this can only be changed by LGBM_SetMaxThreads()
-static int LGBM_MAX_NUM_THREADS = -1;
+static int LGBM_MAX_NUM_THREADS;
 
 // this is modified by OMP_SET_NUM_THREADS(), for example
 // by passing num_thread through params
-static int LGBM_DEFAULT_NUM_THREADS = -1;
+static int LGBM_DEFAULT_NUM_THREADS;
 
 /*
     Get number of threads to use in OpenMP parallel regions.
@@ -36,35 +36,10 @@ static int LGBM_DEFAULT_NUM_THREADS = -1;
       - https://www.openmp.org/spec-html/5.0/openmpsu112.html
       - https://gcc.gnu.org/onlinedocs/libgomp/omp_005fget_005fmax_005fthreads.html
 */
-static int OMP_NUM_THREADS() {
-  int default_num_threads;
+static int OMP_NUM_THREADS();
 
-  if (LGBM_DEFAULT_NUM_THREADS > 0) {
-    // if LightGBM-specific default has been set, ignore OpenMP-global config
-    default_num_threads = LGBM_DEFAULT_NUM_THREADS;
-  } else {
-    // otherwise, default to OpenMP-global config
-    #pragma omp parallel
-    #pragma omp master
-    { default_num_threads = omp_get_max_threads(); }
-  }
+static void OMP_SET_NUM_THREADS(int num_threads);
 
-  // ensure that if LGBM_SetMaxThreads() was ever called, LightGBM doesn't
-  // use more than that many threads
-  if (LGBM_MAX_NUM_THREADS > 0 and default_num_threads > LGBM_MAX_NUM_THREADS) {
-    return LGBM_MAX_NUM_THREADS;
-  }
-
-  return default_num_threads;
-}
-
-static void OMP_SET_NUM_THREADS(int num_threads) {
-  if (num_threads <= 0) {
-    LGBM_DEFAULT_NUM_THREADS = -1;
-  } else {
-    LGBM_DEFAULT_NUM_THREADS = num_threads;
-  }
-}
 
 class ThreadExceptionHelper {
  public:
